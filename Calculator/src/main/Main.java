b655a0398f234a12fa9166dad6666e48e3a9eb26
Ernest <Ernest@LAPTOP-U62S79TP>
2019-05-01 15:20:48 +0200

    calculate value of bracket

--- Calculator/src/main/Main.java
+++ Calculator/src/main/Main.java
@@ -1,5 +1,11 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import model.Operation;
+
 public class Main {
 
 	public static void main(String[] args) {
